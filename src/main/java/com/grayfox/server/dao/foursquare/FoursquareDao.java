c84372b3d0e89371e542ec3aa289a20fdbbe71da
dan-zx <danyboy_shaggy@hotmail.com>
2015-07-31 11:34:34 -0500

    Moved init logic to FoursquareDao

--- src/main/java/com/grayfox/server/dao/foursquare/FoursquareDao.java
+++ src/main/java/com/grayfox/server/dao/foursquare/FoursquareDao.java
@@ -15,15 +15,88 @@
  */
 package com.grayfox.server.dao.foursquare;
 
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+
+import javax.annotation.PostConstruct;
 import javax.inject.Inject;
 
+import com.foursquare4j.response.Result;
+import com.grayfox.server.dao.DaoException;
+
+import com.grayfox.server.domain.Category;
 import com.foursquare4j.FoursquareApi;
 
 abstract class FoursquareDao {
 
+    private Map<String, Category> categoryCatalog;
+
     @Inject private FoursquareApi foursquareApi;
+
+    @PostConstruct
+    protected void init() {
+        categoryCatalog = allCategories();
+    }
 
     protected FoursquareApi getFoursquareApi() {
         return foursquareApi;
+    }
+
+    protected Map<String, Category> getCategoryCatalog() {
+        return categoryCatalog;
+    }
+
+    private Map<String, Category> allCategories() {
+        FoursquareApi foursquareApi = getFoursquareApi();
+        Map<String, Category> myCategories = null;
+        Map<String, Category> myCategoriesInSpanish = null;
+        foursquareApi.setLocale(new Locale("en"));
+        Result<com.foursquare4j.response.Category[]> foursquareCategories = foursquareApi.getVenueCategories();
+        foursquareApi.setLocale(new Locale("es"));
+        Result<com.foursquare4j.response.Category[]> foursquareCategoriesInSpanish = foursquareApi.getVenueCategories();
+        foursquareApi.setLocale(null);
+        if (foursquareCategories.getMeta().getCode() == 200) myCategories = toCategories(foursquareCategories.getResponse());
+        else throw new DaoException.Builder()
+                .messageKey("foursquare.request.error")
+                .addMessageArgument(foursquareCategories.getMeta().getErrorDetail())
+                .build();
+        if (foursquareCategoriesInSpanish.getMeta().getCode() == 200) myCategoriesInSpanish = toCategories(foursquareCategoriesInSpanish.getResponse());
+        else throw new DaoException.Builder()
+                .messageKey("foursquare.request.error")
+                .addMessageArgument(foursquareCategoriesInSpanish.getMeta().getErrorDetail())
+                .build();
+
+        for (Map.Entry<String, Category> entry : myCategories.entrySet()) {
+            entry.getValue().setSpanishName(myCategoriesInSpanish.get(entry.getKey()).getDefaultName());
+        }
+
+        return myCategories;
+    }
+
+    private Map<String, Category> toCategories(com.foursquare4j.response.Category[] categories) {
+        Map<String, Category> myCategories = new HashMap<>();
+        for (com.foursquare4j.response.Category foursquareCategory : categories) {
+            Category category = new Category();
+            category.setDefaultName(foursquareCategory.getName());
+            category.setIconUrl(new StringBuilder().append(foursquareCategory.getIcon().getPrefix()).append("88").append(foursquareCategory.getIcon().getSuffix()).toString());
+            category.setFoursquareId(foursquareCategory.getId());
+            myCategories.put(foursquareCategory.getId(), category);
+            for (com.foursquare4j.response.Category foursquareSubCategory : foursquareCategory.getCategories()) {
+                Category subCategory = new Category();
+                subCategory.setDefaultName(foursquareSubCategory.getName());
+                subCategory.setIconUrl(new StringBuilder().append(foursquareCategory.getIcon().getPrefix()).append("88").append(foursquareCategory.getIcon().getSuffix()).toString());
+                subCategory.setFoursquareId(foursquareSubCategory.getId());
+                myCategories.put(foursquareSubCategory.getId(), subCategory);
+                for (com.foursquare4j.response.Category foursquareSubSubCategory : foursquareSubCategory.getCategories()) {
+                    Category subSubCategory = new Category();
+                    subSubCategory.setDefaultName(foursquareSubSubCategory.getName());
+                    subSubCategory.setIconUrl(new StringBuilder().append(foursquareCategory.getIcon().getPrefix()).append("88").append(foursquareCategory.getIcon().getSuffix()).toString());
+                    subSubCategory.setFoursquareId(foursquareSubSubCategory.getId());
+                    myCategories.put(foursquareSubSubCategory.getId(), subSubCategory);
+                }
+            }
+        }
+        return myCategories;
     }
 }
--- src/main/java/com/grayfox/server/dao/foursquare/PoiFoursquareDao.java
+++ src/main/java/com/grayfox/server/dao/foursquare/PoiFoursquareDao.java
@@ -17,16 +17,9 @@ package com.grayfox.server.dao.foursquare;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Locale;
-import java.util.Map;
 import java.util.Set;
-
-import javax.annotation.PostConstruct;
-
-import org.springframework.stereotype.Repository;
 
 import com.foursquare4j.FoursquareApi;
 import com.foursquare4j.response.ExploreVenueGroups;
@@ -40,15 +33,10 @@ import com.grayfox.server.domain.Category;
 import com.grayfox.server.domain.Location;
 import com.grayfox.server.domain.Poi;
 
+import org.springframework.stereotype.Repository;
+
 @Repository
 public class PoiFoursquareDao extends FoursquareDao {
-
-    private Map<String, Category> categories;
-
-    @PostConstruct
-    protected void init() {
-        categories = allCategories();
-    }
 
     public List<Poi> fetchNearLocations(Location... locations) {
         FoursquareApi foursquareApi = getFoursquareApi();
@@ -86,63 +74,10 @@ public class PoiFoursquareDao extends FoursquareDao {
         poi.setFoursquareRating(venue.getRating());
         Set<Category> myCategories = new HashSet<>();
         for (com.foursquare4j.response.Category category : venue.getCategories()) {
-            Category myCategory = categories.get(category.getId());
+            Category myCategory = getCategoryCatalog().get(category.getId());
             myCategories.add(myCategory);
         }
         poi.setCategories(myCategories);
         return poi;
-    }
-
-    private Map<String, Category> allCategories() {
-        FoursquareApi foursquareApi = getFoursquareApi();
-        Map<String, Category> myCategories = null;
-        Map<String, Category> myCategoriesInSpanish = null;
-        foursquareApi.setLocale(new Locale("en"));
-        Result<com.foursquare4j.response.Category[]> foursquareCategories = foursquareApi.getVenueCategories();
-        foursquareApi.setLocale(new Locale("es"));
-        Result<com.foursquare4j.response.Category[]> foursquareCategoriesInSpanish = foursquareApi.getVenueCategories();
-        foursquareApi.setLocale(null);
-        if (foursquareCategories.getMeta().getCode() == 200) myCategories = toCategories(foursquareCategories.getResponse());
-        else throw new DaoException.Builder()
-                .messageKey("foursquare.request.error")
-                .addMessageArgument(foursquareCategories.getMeta().getErrorDetail())
-                .build();
-        if (foursquareCategoriesInSpanish.getMeta().getCode() == 200) myCategoriesInSpanish = toCategories(foursquareCategoriesInSpanish.getResponse());
-        else throw new DaoException.Builder()
-                .messageKey("foursquare.request.error")
-                .addMessageArgument(foursquareCategoriesInSpanish.getMeta().getErrorDetail())
-                .build();
-
-        for (Map.Entry<String, Category> entry : myCategories.entrySet()) {
-            entry.getValue().setSpanishName(myCategoriesInSpanish.get(entry.getKey()).getDefaultName());
-        }
-
-        return myCategories;
-    }
-
-    private Map<String, Category> toCategories(com.foursquare4j.response.Category[] categories) {
-        Map<String, Category> myCategories = new HashMap<>();
-        for (com.foursquare4j.response.Category foursquareCategory : categories) {
-            Category category = new Category();
-            category.setDefaultName(foursquareCategory.getName());
-            category.setIconUrl(new StringBuilder().append(foursquareCategory.getIcon().getPrefix()).append("88").append(foursquareCategory.getIcon().getSuffix()).toString());
-            category.setFoursquareId(foursquareCategory.getId());
-            myCategories.put(foursquareCategory.getId(), category);
-            for (com.foursquare4j.response.Category foursquareSubCategory : foursquareCategory.getCategories()) {
-                Category subCategory = new Category();
-                subCategory.setDefaultName(foursquareSubCategory.getName());
-                subCategory.setIconUrl(new StringBuilder().append(foursquareCategory.getIcon().getPrefix()).append("88").append(foursquareCategory.getIcon().getSuffix()).toString());
-                subCategory.setFoursquareId(foursquareSubCategory.getId());
-                myCategories.put(foursquareSubCategory.getId(), subCategory);
-                for (com.foursquare4j.response.Category foursquareSubSubCategory : foursquareSubCategory.getCategories()) {
-                    Category subSubCategory = new Category();
-                    subSubCategory.setDefaultName(foursquareSubSubCategory.getName());
-                    subSubCategory.setIconUrl(new StringBuilder().append(foursquareCategory.getIcon().getPrefix()).append("88").append(foursquareCategory.getIcon().getSuffix()).toString());
-                    subSubCategory.setFoursquareId(foursquareSubSubCategory.getId());
-                    myCategories.put(foursquareSubSubCategory.getId(), subSubCategory);
-                }
-            }
-        }
-        return myCategories;
     }
 }
